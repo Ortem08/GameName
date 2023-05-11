c2c04b66731ebcab85e21cf78058f66646c2d168
VladimirLezhnin <lezhnin.vi@mail.ru>
2023-05-11 22:20:11 +0500

    Add Tutorial Messages for Tutorial Level
    Add Tutorial Messages for Tutorial Level

--- Assets/Scripts/PressedButtonActivate.cs
+++ Assets/Scripts/PressedButtonActivate.cs
@@ -1,16 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PressedButtonActivate : MonoBehaviour
 {
     private Inventory _inventory;
     public int Id;
+    private List<string> tutorialMessages;
+    private int currentTutorMessageIndex;
 
     // Start is called before the first frame update
     void Start()
     {
         _inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        tutorialMessages = new List<string>() { "Для передвижения используйте левую кнопку мыши", 
+            "Чтобы подабрать предмет, подойдите к нему достаточно близко и нажмите кнопку F", 
+            "Чтобы использовать предметы в первой, второй и третьей ячейках нажимайте клавиши Q, W, E соответственно", 
+            "Для некоторых предметов, после их использования, вам понадобится указать область действия с помощью правой кнопки мыши", 
+            "Для того, чтобы пройти обучающий уровень, используйте знания, полученные в этом обучении и нанесите урон одному из нпс и уменьшите его хп до нуля", 
+            "" };
+        currentTutorMessageIndex = 0;
     }
     
     public void DropItem()
@@ -18,5 +31,22 @@ public class PressedButtonActivate : MonoBehaviour
         _inventory.isFull[Id] = false;
         if (transform.childCount > 0)
             transform.GetChild(0).GetComponent<Spawn>().SpawnDroppedItem();
+    }
+
+    public void GiveNextTutorialMessage()
+    {
+        var textBox = GameObject.FindGameObjectWithTag("TutorialMessage");
+        var buttonText = textBox.GetComponentInChildren<Button>();
+        var text = textBox.GetComponentInChildren<TextMeshProUGUI>();
+        text.text = tutorialMessages[currentTutorMessageIndex];
+        currentTutorMessageIndex++;
+        if (currentTutorMessageIndex == tutorialMessages.Count - 1)
+        {
+            buttonText.GetComponentInChildren<TextMeshProUGUI>().text = "Закрыть";
+        }
+        if (currentTutorMessageIndex >= tutorialMessages.Count)
+        {
+            Destroy(textBox);
+        }
     }
 }
