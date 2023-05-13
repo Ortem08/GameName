b4eb8c8e222ac561ac28993c0c0ccd6113dbca7b
Ortem08 <metrocel@mail.ru>
2023-05-13 12:29:04 +0500

    Add settings saving

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -35,7 +35,6 @@ public class PlayerControl : MonoBehaviour
     
     private void Update()
     {
-        Debug.Log(QualitySettings.GetQualityLevel());
         SetTargetPosition();
     }
 
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -7,6 +9,36 @@ using UnityEngine.UI;
 public class Settings : MonoBehaviour
 {
     public AudioMixer AM;
+
+    public Toggle FullScreen;
+    public Slider Volume;
+    public TMP_Dropdown Quality;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("FullScreenValue"))
+        {
+            var fsVal = Convert.ToBoolean(PlayerPrefs.GetInt("FullScreenValue"));
+            FullScreen.isOn = fsVal;
+            Screen.fullScreen = fsVal;
+        }
+
+        if (PlayerPrefs.HasKey("VolumeValue"))
+        {
+            var volVal = PlayerPrefs.GetFloat("VolumeValue");
+            Volume.value = volVal;
+            AM.SetFloat("MasterVolume", volVal);
+        }
+
+        if (PlayerPrefs.HasKey("QualityValue"))
+        {
+            var qulVal = PlayerPrefs.GetInt("QualityValue");
+            Quality.value = qulVal;
+            QualitySettings.SetQualityLevel(qulVal);
+        }
+    }
+        
+
     //public Dropdown dropdown;
 
     //private Resolution[] rsl;
@@ -31,5 +63,18 @@ public class Settings : MonoBehaviour
     public void SetQuality(int q)
     {
         QualitySettings.SetQualityLevel(q);
+    }
+
+    public void SaveSettings()
+    {
+        var intFullScreen = FullScreen ? 1 : 0;
+        
+        PlayerPrefs.SetInt("FullScreenValue", intFullScreen);
+        PlayerPrefs.SetFloat("VolumeValue", Volume.value);
+        PlayerPrefs.SetInt("QualityValue", Quality.value);
+
+        Debug.Log(intFullScreen);
+        Debug.Log(Volume.value);
+        Debug.Log(Quality.value);
     }
 }
