95ce582082923eb26761772c0ba4c6ec0dcae23e
Ortem08 <metrocel@mail.ru>
2023-06-02 08:34:56 +0500

    Add Shtirlic

--- Assets/Scripts/Blink.cs
+++ Assets/Scripts/Blink.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Blink : MonoBehaviour
 {
     public RawImage Picture;
+    private AudioSource source;
+    private AudioClip clip;
+    private bool flag;
 
     void Start()
     {
-        Picture = gameObject.GetComponent<RawImage>();
+        source = gameObject.AddComponent<AudioSource>();
+        clip = Resources.Load<AudioClip>("Sounds/Shtirlic");
+        source.PlayOneShot(clip);
+        StartCoroutine(Screamer());
     }
 
-    public void Update()
+    private void Update()
     {
-        Picture.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, 0.5f));
+        if (flag)
+            Picture.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, 0.5f));
+    }
+
+    public IEnumerator Screamer()
+    {
+        yield return new WaitForSeconds(clip.length);
+        Picture.enabled = true;
+        flag = true;
     }
 }
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,8 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.Burst.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
