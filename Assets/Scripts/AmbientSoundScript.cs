fb5906bcc2330e8974a06734734358513c25b482
Ortem08 <metrocel@mail.ru>
2023-06-02 05:56:04 +0500

    Add music

--- Assets/Scripts/AmbientSoundScript.cs
+++ Assets/Scripts/AmbientSoundScript.cs
@@ -8,13 +8,14 @@ public class AmbientSoundScript : MonoBehaviour
 {
     private List<AudioClip> ambientSounds;
     private AudioSource source;
+    private AudioSource source2;
     private AudioMixer Mixer { get; set; }
     private int soundsCount;
     private System.Random random;
     
     void Start()
     {
-        soundsCount = 180;
+        soundsCount = 176;
         random = new System.Random();
         Mixer = Resources.Load<AudioMixer>("AudioMixer");
         ambientSounds = new List<AudioClip>();
@@ -25,7 +26,9 @@ public class AmbientSoundScript : MonoBehaviour
             ambientSounds.Add(sickSoundClip);
         }
 
-        source = gameObject.AddComponent<AudioSource>();
+        source = gameObject.AddComponent<AudioSource>(); 
+        source2 = gameObject.AddComponent<AudioSource>();
+        source2.volume = 0.5f;
         source.outputAudioMixerGroup = Mixer.FindMatchingGroups("Master")[0];
         source.clip = nightSoundClip;
         source.loop = true;
@@ -36,19 +39,12 @@ public class AmbientSoundScript : MonoBehaviour
 
     }
 
-   
-    void Update()
-    {
-
-    }
-
     private IEnumerator WaitAndPlayAmbientSound()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(20);
         var clip = ambientSounds[random.Next(0, soundsCount - 1)];
-        source.PlayOneShot(clip);
+        source2.PlayOneShot(clip);
 
         StartCoroutine(WaitAndPlayAmbientSound());
-
     }
 }
